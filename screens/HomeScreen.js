7fd915f31a0bebc2df9a67229b6aa6e2de093140
Rafael Melón <info@rafaelmelon.es>
2020-04-03 16:56:19 +0200

    feat: covid data countries

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -11,58 +11,245 @@ import {
   FlatList,
   Picker
 } from "react-native";
-
+import { Ionicons } from "@expo/vector-icons";
 import { ScrollView } from "react-native-gesture-handler";
 
 import { MonoText } from "../components/StyledText";
 
-export default function HomeScreen() {
-  const [selectedValue, setSelectedValue] = React.useState("Spain");
-  const [isLoading, setLoading] = React.useState(true);
-  const [data, setData] = React.useState([]);
+const SPAIN = "Spain";
 
-  const [countries, setCountries] = React.useState({});
+export default function HomeScreen() {
+  const [selectedValue, setSelectedValue] = React.useState(SPAIN);
+
+  const [countries, setCountries] = React.useState([]);
   const [countriesData, setCountriesData] = React.useState({});
-  const [countryData, setCountryData] = React.useState([]);
+  const [countryData, setCountryData] = React.useState(null);
+  const [globalData, setGlobalData] = React.useState(null);
+
+  const [isLoadingCountries, setLoadingCountries] = React.useState(true);
+  const [isLoadingTimeSeries, setLoadingTimeseries] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch("https://pomber.github.io/covid19/timeseries.json")
+      .then(response => response.json())
+      .then(json => {
+        const confirmed = Object.keys(json).reduce(
+          (a, b) => a + json[b][json[b].length - 1].confirmed,
+          0
+        );
+        const deaths = Object.keys(json).reduce(
+          (a, b) => a + json[b][json[b].length - 1].deaths,
+          0
+        );
+
+        const recovered = Object.keys(json).reduce(
+          (a, b) => a + json[b][json[b].length - 1].recovered,
+          0
+        );
+
+        setGlobalData({
+          date: "date",
+          confirmed,
+          deaths,
+          recovered
+        });
+        setCountryData(getCountryData(json[SPAIN]));
+        return setCountriesData(json);
+      })
+      .catch(error => console.error(error))
+      .finally(() => setLoadingTimeseries(false));
+  }, []);
 
   React.useEffect(() => {
     fetch("https://pomber.github.io/covid19/countries.json")
       .then(response => response.json())
       .then(json => {
-        const peopleArray = Object.keys(json).map(i => ({
+        const formatJson = Object.keys(json).map(i => ({
           name: i,
           flag: json[i].flag,
           code: json[i].code
         }));
 
-        return setCountries(peopleArray);
+        return setCountries(formatJson);
       })
       .catch(error => console.error(error))
-      .finally(() => setLoading(false));
+      .finally(() => setLoadingCountries(false));
   }, []);
 
-  React.useEffect(() => {
-    // fetch("https://reactnative.dev/movies.json")
-    //   .then(response => response.json())
-    //   .then(json => setData(json.movies))
-    //   .catch(error => console.error(error))
-    //   .finally(() => setLoading(false));
-
-    fetch("https://pomber.github.io/covid19/timeseries.json")
-      .then(response => response.json())
-      .then(json => setCountriesData(json))
-      .catch(error => console.error(error))
-      .finally(() => setLoading(false));
-  }, []);
-
-  const handleChangeCountry = country => {
-    const countryArray = countriesData[country];
-
-    setSelectedValue(country);
-    setCountryData(countryArray[countryArray.length - 1]);
+  const renderPickerItems = () => {
+    const options = countries.map(country => (
+      <Picker.Item
+        key={country.name}
+        label={`${country.flag} ${country.name}`}
+        value={country.name}
+      />
+    ));
+    return options;
   };
 
-  console.log(countries);
+  const getIconId = (a, b) => {
+    if (a > b) {
+      return "md-arrow-round-up";
+    }
+    if (a < b) {
+      return "md-arrow-round-down";
+    }
+    return "md-arrow-round-forward";
+  };
+
+  const getIconColor = iconId => {
+    if (iconId === "md-arrow-round-up") {
+      return "red";
+    }
+    if (iconId === "md-arrow-round-down") {
+      return "green";
+    }
+    return "grey";
+  };
+
+  const getCountryData = array => {
+    if (array) {
+      const lastData = array[array.length - 1];
+      const secondLastData = array[array.length - 2];
+
+      return {
+        date: lastData.date,
+        confirmed: {
+          last: lastData.confirmed,
+          secondLast: secondLastData.confirmed,
+          iconId: getIconId(lastData.confirmed, secondLastData.confirmed)
+        },
+        deaths: {
+          last: lastData.deaths,
+          secondLast: secondLastData.deaths,
+          iconId: getIconId(lastData.deaths, secondLastData.deaths)
+        },
+        recovered: {
+          last: lastData.recovered,
+          secondLast: secondLastData.recovered,
+          iconId: getIconId(lastData.recovered, secondLastData.recovered)
+        }
+      };
+    }
+
+    return null;
+  };
+
+  const handleChangeCountry = country => {
+    const countryDataArray = countriesData[country];
+    const data = getCountryData(countryDataArray);
+
+    setSelectedValue(country);
+    setCountryData(data);
+  };
+
+  const renderCountryData = () => {
+    if (countryData) {
+      const { date, confirmed, deaths, recovered } = countryData;
+      return (
+        <>
+          <View style={styles.getStatContainer}>
+            <MonoText
+              style={styles.codeHighlightText}
+            >{`Last updated: ${date}`}</MonoText>
+          </View>
+
+          <View style={styles.getStatContainer}>
+            <Text style={styles.getTitleText}>Cases:</Text>
+            <View style={styles.getValueContainer}>
+              <Text style={styles.getCasesText}>{confirmed.last}</Text>
+              <Ionicons
+                style={{ marginHorizontal: 5 }}
+                color={getIconColor(confirmed.iconId)}
+                name={confirmed.iconId}
+                size={20}
+              />
+              <Text style={styles.getSecondLast}>{confirmed.secondLast}</Text>
+            </View>
+          </View>
+
+          <View style={styles.getStatContainer}>
+            <Text style={styles.getTitleText}>Deaths:</Text>
+            <View style={styles.getValueContainer}>
+              <Text style={styles.getDeathsText}>{deaths.last}</Text>
+              <Ionicons
+                style={{ marginHorizontal: 5 }}
+                color={getIconColor(deaths.iconId)}
+                name={deaths.iconId}
+                size={20}
+              />
+              <Text style={styles.getSecondLast}>{deaths.secondLast}</Text>
+            </View>
+          </View>
+
+          <View style={styles.getStatContainer}>
+            <Text style={styles.getTitleText}>Recovered:</Text>
+            <View style={styles.getValueContainer}>
+              <Text style={styles.getRecoveredText}>{recovered.last}</Text>
+              <Ionicons
+                style={{ marginHorizontal: 5 }}
+                color={getIconColor(recovered.iconId)}
+                name={recovered.iconId}
+                size={20}
+              />
+              <Text style={styles.getSecondLast}>{recovered.secondLast}</Text>
+            </View>
+          </View>
+        </>
+      );
+    }
+
+    return (
+      <View style={styles.getStatContainer}>
+        <Text style={styles.getTitleText}>NO DATA</Text>
+      </View>
+    );
+  };
+
+  const renderGlobalData = () => {
+    if (globalData) {
+      const { confirmed, deaths, recovered } = globalData;
+      return (
+        <>
+          <View style={styles.welcomeContainer}>
+            <Image
+              source={require("../assets/images/covid19.png")}
+              style={styles.welcomeImage}
+            />
+          </View>
+
+          <View style={styles.getStatContainer}>
+            <MonoText
+              style={styles.codeHighlightText}
+            >{`Last updated: 12312312`}</MonoText>
+          </View>
+
+          <View style={styles.getStatContainer}>
+            <Text style={styles.getTitleText}>Cases:</Text>
+            <Text style={styles.getCasesText}>{confirmed}</Text>
+          </View>
+
+          <View style={styles.getStatContainer}>
+            <Text style={styles.getTitleText}>Deaths:</Text>
+            <Text style={styles.getDeathsText}>{deaths}</Text>
+          </View>
+
+          <View style={styles.getStatContainer}>
+            <Text style={styles.getTitleText}>Recovered:</Text>
+            <Text style={styles.getRecoveredText}>{recovered}</Text>
+          </View>
+        </>
+      );
+    }
+
+    return (
+      <View style={styles.getStatContainer}>
+        <Text style={styles.getTitleText}>NO DATA</Text>
+      </View>
+    );
+  };
+
+  console.log(globalData);
 
   return (
     <View style={styles.container}>
@@ -70,91 +257,26 @@ export default function HomeScreen() {
         style={styles.container}
         contentContainerStyle={styles.contentContainer}
       >
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require("../assets/images/covid19.png")
-                : require("../assets/images/covid19.png")
-            }
-            style={styles.welcomeImage}
-          />
-          <Text style={styles.tabBarInfoText}>
-            Last updated: April 01, 2020, 17:34 GMT
-          </Text>
-        </View>
+        {isLoadingTimeSeries ? <ActivityIndicator /> : renderGlobalData()}
 
-        {isLoading ? (
+        {isLoadingCountries || countries.length === 0 ? (
           <ActivityIndicator />
         ) : (
-          <FlatList
-            data={data}
-            keyExtractor={({ id }, index) => id}
-            renderItem={({ item }) => (
-              <Text>
-                {item.title}, {item.releaseYear}
-              </Text>
-            )}
-          />
+          <View style={styles.getSelectPickerContainer}>
+            <Picker
+              selectedValue={selectedValue}
+              style={styles.getSelectPicker}
+              onValueChange={(itemValue, itemIndex) =>
+                handleChangeCountry(itemValue)
+              }
+            >
+              {renderPickerItems()}
+            </Picker>
+          </View>
         )}
 
-        <View style={styles.getStatContainer}>
-          <Text style={styles.getTitleText}>Coronavirus Cases:</Text>
-          <Text style={styles.getCasesText}>912,098</Text>
-        </View>
-
-        <View style={styles.getStatContainer}>
-          <Text style={styles.getTitleText}>Deaths:</Text>
-          <Text style={styles.getDeathsText}>45,540</Text>
-        </View>
-
-        <View style={styles.getStatContainer}>
-          <Text style={styles.getTitleText}>Recovered:</Text>
-          <Text style={styles.getRecoveredText}>190,933</Text>
-        </View>
-
-        <View style={styles.getSelectPickerContainer}>
-          <Picker
-            selectedValue={selectedValue}
-            style={styles.getSelectPicker}
-            onValueChange={(itemValue, itemIndex) =>
-              handleChangeCountry(itemValue)
-            }
-          >
-            <Picker.Item label="Spain" value="Spain" />
-            <Picker.Item label="Italy" value="Italy" />
-          </Picker>
-        </View>
-
-        <View style={styles.getStatContainer}>
-          <Text style={styles.getTitleText}>Coronavirus Cases:</Text>
-          <Text style={styles.getCasesText}>912,098</Text>
-        </View>
-
-        <View style={styles.getStatContainer}>
-          <Text style={styles.getTitleText}>Deaths:</Text>
-          <Text style={styles.getDeathsText}>45,540</Text>
-        </View>
-
-        <View style={styles.getStatContainer}>
-          <Text style={styles.getTitleText}>Recovered:</Text>
-          <Text style={styles.getRecoveredText}>190,933</Text>
-        </View>
+        {isLoadingTimeSeries ? <ActivityIndicator /> : renderCountryData()}
       </ScrollView>
-
-      {/* <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          This is a tab bar. You can edit it in:
-        </Text>
-
-        <View
-          style={[styles.codeHighlightContainer, styles.navigationFilename]}
-        >
-          <MonoText style={styles.codeHighlightText}>
-            navigation/BottomTabNavigator.js
-          </MonoText>
-        </View>
-      </View> */}
     </View>
   );
 }
@@ -211,7 +333,7 @@ const styles = StyleSheet.create({
   getSelectPicker: {
     width: 200,
     alignItems: "center",
-    backgroundColor: "#aaa"
+    borderColor: "#aaa"
   },
   developmentModeText: {
     marginBottom: 20,
@@ -238,13 +360,19 @@ const styles = StyleSheet.create({
   },
   getStatContainer: {
     alignItems: "center",
-    marginHorizontal: 50
+    marginHorizontal: 50,
+    marginBottom: 10
+  },
+  getValueContainer: {
+    alignItems: "center",
+    flexDirection: "row"
   },
   homeScreenFilename: {
     marginVertical: 7
   },
   codeHighlightText: {
-    color: "rgba(96,100,109, 0.8)"
+    color: "rgba(96,100,109, 0.8)",
+    marginBottom: 5
   },
   codeHighlightContainer: {
     backgroundColor: "rgba(0,0,0,0.05)",
@@ -265,27 +393,33 @@ const styles = StyleSheet.create({
     textAlign: "center",
     marginBottom: 5
   },
+  getSecondLast: {
+    fontSize: 14,
+    color: "rgba(96,100,109, 1)",
+    lineHeight: 24,
+    textAlign: "center"
+  },
   getCasesText: {
-    fontSize: 30,
+    fontSize: 35,
     fontWeight: "bold",
     color: "#aaa",
-    lineHeight: 30,
+    lineHeight: 35,
     textAlign: "center",
     marginBottom: 5
   },
   getDeathsText: {
-    fontSize: 30,
+    fontSize: 35,
     fontWeight: "bold",
     color: "#696969",
-    lineHeight: 30,
+    lineHeight: 35,
     textAlign: "center",
     marginBottom: 5
   },
   getRecoveredText: {
-    fontSize: 30,
+    fontSize: 35,
     fontWeight: "bold",
     color: "#8ACA2B",
-    lineHeight: 30,
+    lineHeight: 35,
     textAlign: "center",
     marginBottom: 5
   },
